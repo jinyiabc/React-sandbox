e30778b79b19cef3644c759cbd30dab37d03baf0
Alex J.Y <13817282645@163.com>
2018-04-14 19:19:23 +0800

    3-declaring winner

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+//return early and ignore the click
+//if someone has already won the game or
+//if a square is already filled:
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }    
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({squares: squares,
         xIsNext: !this.state.xIsNext
@@ -37,7 +43,13 @@ class Board extends React.Component {
   }
 
   render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
 
     return (
       <div>
@@ -79,6 +91,26 @@ class Game extends React.Component {
 }
 export default Game;
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    // Destructuring Assignment
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 //
 // ReactDOM.render(
